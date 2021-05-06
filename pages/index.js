cd58dee711eec0bfd58e905247cacfdef8e676dc
noureddine <nor@LAPTOP-48MA8VKR.localdomain>
2021-05-06 15:18:19 +0200

    added title to home page

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,20 @@ import styles from '../styles/Home.module.css'
 import ProductScrollBar from '../components/ProductScrollBar/ProductScrollBar'
 import HistoryDiv from '../components/HistoryDiv/HistoryDiv'
 import ShopDiv from '../components/ShopDiv/ShopDiv'
+import {Text} from '@chakra-ui/react';
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+      <title>vieille sardine</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
+      <Text fontSize="50px" color="teal">
+      vieille sardine
+      </Text>
         <div>
           <b>Livraison offert à partir de 126 € d’achat en France métropolitaine, Belgique, Hollande</b>
         </div>
