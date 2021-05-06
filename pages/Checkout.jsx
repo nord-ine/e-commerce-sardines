0079dbd0683876e74ae2d762378c602d00e1a1e3
noureddine <nor@LAPTOP-48MA8VKR.localdomain>
2021-05-06 04:31:22 +0200

    fixed bugs related to payment page

--- pages/Checkout.jsx
+++ pages/Checkout.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import { Center,VStack,Heading,Text,Input, FormControl,Button,
-    FormLabel,
-    FormErrorMessage,
-    HStack,} from "@chakra-ui/react"
 
-import {FiLogIn} from 'react-icons/fi'
-import { useForm } from "react-hook-form";
-import { gql, useMutation } from '@apollo/client';
-import { useRouter } from 'next/router';
-import {useState} from 'react';
+const Checkout = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
 
-
-
-
+export default Checkout
